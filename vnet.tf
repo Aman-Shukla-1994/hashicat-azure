dde2c2f7d930a6c69e51fb2eb31c2a242871b3a7
Aman Shukla <amanadityashukla@gmail.com>
2021-09-27 18:14:39 +0000

    Added vnet module2

--- vnet.tf
+++ vnet.tf
@@ -1,5 +1,5 @@
 module "network" {
   source  = "app.terraform.io/AmanShukla-training/network/azurerm"
   version = "3.0.1"
-  resource_group_name = "testRG"
+  resource_group_name = "azurerm_resource_group"
 }
